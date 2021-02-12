c21475618bbbfd6106c29fa918ed30d392f11aee
karttur <thomas.gumbricht@gmail.com>
2021-02-12 09:21:33 +0100

    import updates

--- __init__.py
+++ __init__.py
@@ -1,4 +1,7 @@
 """
+Created 22 Jan 2021
+Last updated 12 Feb 2021
+
 Karttur Landsat specific processing
 
 Author
@@ -9,3 +12,5 @@ Thomas Gumbricht
 from .version import __version__, VERSION
 
 from .landsat import ProcessLandsat
+
+__all__ = ['ProcessLandsat']
